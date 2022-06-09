252d67c2bd32ded3af823107f400cb87e9bfced7
Jônatas Felix <jon.cunha@hotmail.com>
2022-06-09 16:06:31 -0300

    Atividade dia 09-06

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
-import Post from './components/Post/Post';
+import SecaoPostar from './components/SecaoPostar/SecaoPostar';
 
 const MainContainer = styled.div`
   display: flex;
@@ -13,21 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'Paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?a=1'}
-          fotoPost={'https://picsum.photos/200/150?a=2'}
-        />
-         <Post
-          nomeUsuario={'Felipe'}
-          fotoUsuario={'https://picsum.photos/50/50?a=3'}
-          fotoPost={'https://picsum.photos/200/150?a=4'}
-        />
-         <Post
-          nomeUsuario={'Henrique'}
-          fotoUsuario={'https://picsum.photos/50/50?a=5'}
-          fotoPost={'https://picsum.photos/200/150?a=6'}
-        />
+        <SecaoPostar/>
       </MainContainer>
     );
   }
