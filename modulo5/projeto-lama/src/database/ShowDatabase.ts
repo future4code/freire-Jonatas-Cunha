ebc3b8fbe28943bc0a5d8e024705e13d8d76483a
Jônatas Felix <jon.cunha@hotmail.com>
2022-09-29 17:34:56 -0300

    Projeto - LAMA - Music Awards

---
+++ modulo5/projeto-lama/src/database/ShowDatabase.ts
@@ -0,0 +1,6 @@
+import { BaseDatabase } from "./BaseDatabase"
+
+export class ShowDatabase extends BaseDatabase {
+    public static TABLE_SHOWS = "Lama_Shows"
+    public static TABLE_TICKETS = "Lama_Tickets"
+}
---
+++ modulo5/projeto-lama/src/models/Show.ts
@@ -0,0 +1,38 @@
+export interface IShowDB {
+    id: string,
+    band: string,
+    starts_at: Date
+}
+
+export interface ITicketDB {
+    id: string,
+    show_id: string,
+    user_id: string
+}
+
+export class Show {
+
+    constructor(
+        private id: string,
+        private band: string,
+        private startsAt: Date,
+        private tickets: number = 5000
+    ) { }
+
+    // Getters
+    public getId = () => this.id
+    public getBand = () => this.band
+    public getStartsAt = () => this.startsAt
+    public getTickets = () => this.tickets
+
+    // Setters
+    public setId = (newId: string) => { this.id = newId }
+    public setBand = (newBand: string) => { this.band = newBand }
+    public setStartsAt = (newStartsAt: Date) => { this.startsAt = newStartsAt }
+    public setTickets = (newTickets: number) => { this.tickets = newTickets }
+
+    // Static methods - convert to model
+    public static toShowModel = (show: any): Show => {
+        return new Show(show.id, show.band, show.starts_at, show.tickets)
+    }
+}
